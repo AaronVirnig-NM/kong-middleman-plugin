d3d53777a224d811f4f553bd8859020b3da04274
smonda5 <48529414+smonda5@users.noreply.github.com>
2019-03-18 10:12:53 -0500

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -4,7 +4,6 @@ return {
     url = {required = true, type = "string"},
     response = { required = true, default = "table", type = "string", enum = {"table", "string"}},
     timeout = { default = 10000, type = "number" },
-    keepalive = { default = 60000, type = "number" },
-    http_method = { default = "POST", type = "string", enum = {"GET", "POST"}}
+    keepalive = { default = 60000, type = "number" }
   }
 }
